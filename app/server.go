639c252f7d887d58bd4df9924f2d21a519f1616e
kriyanshi shah <kriyanshishah06@gmail.com>
2024-03-10 13:22:19 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -12,15 +12,16 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	connection.Write([]byte("+Pong\r\n"))
 }
