90b47dffd04f7c34a5c719e447939641106977d1
kriyanshi shah <kriyanshishah06@gmail.com>
2024-03-11 20:38:34 +0530

    stage 3

--- app/server.go
+++ app/server.go
@@ -13,21 +13,30 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer listener.Close()
+
 	for {
-		buff := make([]byte, 1024)
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(connection)
+	}
+}
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+	buff := make([]byte, 1024)
+	for {
 		n, err := connection.Read(buff)
 		if err != nil || n == 0 {
-			break
+			return
 		}
 		connection.Write([]byte("+PONG\r\n"))
 	}
