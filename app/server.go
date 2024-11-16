5801a358692e4b531ae77d05f21a8a513cd459a0
kriyanshi <kriyanshishah06@gmail.com>
2024-11-16 22:58:49 +0530

    CLI tests (cli-test-1731778129750)

--- app/server.go
+++ app/server.go
@@ -227,7 +227,7 @@ func replicateMaster(address string, store *Store) {
 			}
 		case "replconf":
 			if len(commands) == 3 && commands[1] == "getack" && commands[2] == "*" {
-				masterConn.Write([]byte(fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$%d\r\n", replica.offset)))
+				masterConn.Write([]byte(fmt.Sprintf("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n%d\r\n", replica.offset)))
 			}
 			replica.offset += n
 		default:
